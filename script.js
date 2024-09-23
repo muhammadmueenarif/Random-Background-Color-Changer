74160754324098124f68607f2e39297ade8998bb
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-09-23 22:14:58 +0500

    randm background changer on each refresh using js

---
+++ script.js
@@ -0,0 +1,5 @@
+var colors= ["red", "green", "blue", "orange", "pink", "white","yellow", "purple"];
+
+var newcolor= colors[Math.floor(Math.random() * colors.length)];
+
+document.body.style.background= newcolor;
